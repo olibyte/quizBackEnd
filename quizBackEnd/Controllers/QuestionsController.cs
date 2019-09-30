95dd217001c52dbdd7eaa46514c13d0255e21dfd
Oliver <ocben1@gmail.com>
2019-09-30 22:03:36 +1000

    Add project files.

---
+++ quizBackEnd/Controllers/QuestionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quizBackEnd.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+         [HttpPost]
+        public void Post([FromBody] Models.Question question)
+        {
+        }
+    }
+}
---
+++ quizBackEnd/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quizBackEnd.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+    }
+}
