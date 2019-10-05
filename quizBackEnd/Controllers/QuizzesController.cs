a4ca089f8f05f59693cc73a8c1dd524e2be796df
Oliver <ocben1@gmail.com>
2019-10-06 01:02:33 +1000

    quiz playlist

--- quizBackEnd/Controllers/QuizzesController.cs
+++ quizBackEnd/Controllers/QuizzesController.cs
@@ -31,6 +31,12 @@ namespace quizBackEnd.Controllers
 
             return _context.Quiz.Where(q => q.OwnerId == userId);
         }
+        [HttpGet("all")]
+        public IEnumerable<Quiz> GetAllQuizzes()
+        {
+
+            return _context.Quiz;
+        }
 
         // GET: api/Quizzes/5
         [HttpGet("{id}")]
